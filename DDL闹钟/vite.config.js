d6c83f9fba87a6a6d55234861aeaca27141c7c7a
zouxin <3101522606@qq.com>
2023-01-27 19:46:33 +0800

    fix.

--- DDL闹钟/vite.config.js
+++ DDL闹钟/vite.config.js
@@ -8,7 +8,12 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),],
+  plugins: [vue(), AutoImport({
+    resolvers: [ElementPlusResolver()],
+  }),
+  Components({
+    resolvers: [ElementPlusResolver()],
+  })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
